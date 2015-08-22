f774031168c8351a7ae483675d7203fc27bbe95a
Ted Pyne <ted.pyne.4@gmail.com>
2015-08-22 19:02:13 -0400

    Client tester && correct config

---
+++ client.py
@@ -0,0 +1,7 @@
+import socket
+print "Enter the server address to connect to!"
+ip = raw_input()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(("0.0.0.0",63541))
+s.connect((ip, 8787))
+print s.recv(4096)
