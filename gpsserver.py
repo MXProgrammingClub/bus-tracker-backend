6212ef1c4ba1a5adcee0d258dda82469107b3775
Ted Pyne <ted.pyne.4@gmail.com>
2015-08-21 21:55:39 -0400

    Small changes

--- gpsserver.py
+++ gpsserver.py
@@ -1,8 +1,7 @@
 import os
-import time
 import gps
 import socket     
-    # Listen on port 2947 (gpsd) of localhost
+    
 def main():
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.bind(("0.0.0.0", 8787))
@@ -16,11 +15,12 @@ def main():
       			returnLatLong(c)
       			c[0].close()
 			exit(0)
-		#Wait for someone to ask for a lat/long pair, then returh getLatLong
-def returnLatLong((client, addr)):
+		
+def returnLatLong((client, addr)):	#Wait for someone to ask for a lat/long pair, then returh getLatLong
 	client.send(getLatLong())
-	time.sleep(5);
+
 def getLatLong():     
+	# Listen on port 2947 (gpsd) of localhost
 	session = gps.gps("localhost", "2947")
         session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
 	while True:
