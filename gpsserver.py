1f3fae3201ed058cfd4162702b653097f103e0f8
Ted Pyne <ted.pyne.4@gmail.com>
2015-08-24 12:17:39 -0400

    Much better threading model, UNTESTED

--- gpsserver.py
+++ gpsserver.py
@@ -2,9 +2,8 @@ import os
 import gps
 import socket     
 
-def main():
-	os.system("sudo killall gpsd")
-	os.system("sudo gpsd /dev/ttyUSB0 -F /var/run/gpsd.sock")
+def listen():
+	
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.bind(("0.0.0.0", 8787))
 	s.listen(100)
@@ -19,9 +18,11 @@ def main():
 			exit(0)
 		
 def returnLatLong((client, addr)):	#Wait for someone to ask for a lat/long pair, then returh getLatLong
-	client.send(getLatLong())
+	client.send(data)
 	
-def getLatLong():     
+def setLatLong():     
+	os.system("sudo killall gpsd")
+	os.system("sudo gpsd /dev/ttyUSB0 -F /var/run/gpsd.sock")	
 	# Listen on port 2947 (gpsd) of localhost
 	session = gps.gps("localhost", "2947")
         session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
@@ -30,8 +31,12 @@ def getLatLong():
      			report = session.next()
     			if report['class'] == 'TPV':
 				if hasattr(report, 'lat') and hasattr(report, 'lon'):
-					return "LATITIUDE = " + repr(report.lat) + "\nLONGITUDE = " + repr(report.lon)
+					data = "LATITIUDE = " + repr(report.lat) + "\nLONGITUDE = " + repr(report.lon)
   	   	except KeyError:
     			pass
 
-main()
+str data
+if os.fork():
+	setLatLong()
+else:
+	listen()
