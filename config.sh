244b24dd02ef9c3593c7978be8c6abfbb2454fdb
Ted Pyne <ted.pyne.4@gmail.com>
2015-09-28 21:33:37 -0400

    Refactor client

--- config.sh
+++ config.sh
@@ -3,8 +3,8 @@
 rm -r ../python_games
 sudo apt-get install gpsd gpsd-clients python-gps
 sudo apt-get install python3-pip
-pip install asyncio	
-pip install websockets
+pip3 install asyncio	
+pip3 install websockets
 #Test with "cgps -s"
 
 #Obsolete, onlly use for debugging both on the same computer
