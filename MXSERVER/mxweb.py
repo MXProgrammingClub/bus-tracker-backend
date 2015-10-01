971ed9de82f896c418178fd8ef39355d51dde5e3
Ted Pyne <ted.pyne.4@gmail.com>
2015-10-01 08:45:52 -0400

    Documentation and cleanup

--- MXSERVER/mxweb.py
+++ MXSERVER/mxweb.py
@@ -1,4 +1,3 @@
-
 import socket
 import time
 import thread
@@ -16,18 +15,18 @@ def collectData():
 	s.listen(100) #Accept a maximum of 100 connection simultaneously
 	while True:	    	
 		c = s.accept()	#Accept a new connection
-		thread.start_new_thread( setDataWithClient, (c,))	    		
+		thread.start_new_thread( setDataWithGPS, (c,)) #"Bind" to a GPS
 		
 
-def setDataWithClient(sock):
+def setDataWithGPS(sock):
 	(client,addr) = sock;
 	global dataObj	
 	while True:
-		dataObj.data = client.recv(64).decode("utf-8") ;		
+		dataObj.data = client.recv(64).decode("utf-8");	
 		print dataObj.data;
 		time.sleep(5);	
 
-class TestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+class RequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 	def do_GET(self):
 		print "CALLED!";
 		global dataObj
@@ -36,16 +35,10 @@ class TestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
            	self.end_headers()
 		self.wfile.write(dataObj.data)
 
-def returnLatLong(sock):	#Handle open socket
-	(client,addr)=sock;
-	global dataObj;	
-	print dataObj.data
-	client.send(dataObj.data);
-	client.close();
 
-dataObj = Data();
-thread.start_new_thread( collectData, ())
+dataObj = Data();	#Initiialize global var
+thread.start_new_thread( collectData, ())	#Begin accepting data from GPS
 
 server_address = ("0.0.0.0", 8789)
-server = BaseHTTPServer.HTTPServer(server_address, TestHandler)
-server.serve_forever()
+server = BaseHTTPServer.HTTPServer(server_address, RequestHandler)
+server.serve_forever()		#Start HTTP server to serve clients
